a049818e82483894aa31743e3367b1787f1801d6
michaelhur <jhhur@uwaterloo.ca>
2021-06-02 01:37:35 +0900

    Make sure to bundle, jekyll serve and build

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Github, Jekyll과 Lanyon Plus을 이용하여 블로그 만들기",
+        "excerpt":"선택 장애: 플랫폼 선택하기 블로그를 시작하기로 하면서 플랫폼을 결정하는 것이 가장 힘들었다. 큰 수정 없이 out-of-box로 블로깅을 시작할 수 있는 Wordpress와 Tistory 등의 플랫폼이 존재하였지만 개발을 하면서 익숙해져있던 Github에 더 눈이 갔기때문이다. 개발자라면 누구나 한번쯤은 사용해봤을 Github는 version control, code review등 코딩 환경을 제공해줄뿐만 아니라 Github Pages를 통하여 개인 프로젝트...","categories": ["Etc"],
+        "tags": ["Github","Blog","Jekyll","Lanyon"],
+        "url": "/etc/Blogging-with-github/",
+        "teaser": null
+      }]
